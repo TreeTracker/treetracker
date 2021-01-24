fdff10d157d2caed17c926d9640d27e22625af52
Girish Rajani <girish.r.rajani@gmail.com>
2021-01-24 13:16:42 +0530

    Converted Boot Screen to Splash Screen Like

--- lib/boot.dart
+++ lib/boot.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:treetracker/auth.dart';
 
@@ -10,44 +12,86 @@ class Start extends StatelessWidget {
   }
 }
 
-class BootScreen extends StatelessWidget {
+// class BootScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     // return MaterialApp(
+//     // home: Scaffold(
+//     return Scaffold(
+//       // appBar: AppBar(),
+//       backgroundColor: Colors.grey[900],
+//       body: Container(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Image(image: AssetImage('assets/images/appLogo2.png')),
+//             Padding(
+//               padding: EdgeInsets.all(30),
+//             ),
+//             Text(
+//               'TreeTracker',
+//               style: TextStyle(
+//                 color: Colors.white,
+//                 fontFamily: 'Ubuntu',
+//                 fontSize: 35,
+//               ),
+//             ),
+//             // RaisedButton(
+//             //   child: Text('Continue'),
+//             //   onPressed: () {
+//             //     print("Pressed");
+//             //     Navigator.push(
+//             //       context,
+//             //       MaterialPageRoute(builder: (context) => MyApp()),
+//             //     );
+//             //   },
+//             // ),
+//           ],
+//         ),
+//       ),
+//     );
+//     // );
+//   }
+// }
+
+class BootScreen extends StatefulWidget {
+  @override
+  _BootScreenState createState() => _BootScreenState();
+}
+
+class _BootScreenState extends State<BootScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.push(
+            context, MaterialPageRoute(builder: (context) => MyApp())));
+  }
+
   @override
   Widget build(BuildContext context) {
-    // return MaterialApp(
-    // home: Scaffold(
     return Scaffold(
-      // appBar: AppBar(),
-      backgroundColor: Colors.grey[900],
       body: Container(
+        color: Colors.grey[900],
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image(image: AssetImage('assets/images/appLogo2.png')),
-            Padding(
-              padding: EdgeInsets.all(30),
-            ),
-            Text(
-              'TreeTracker',
-              style: TextStyle(
-                color: Colors.white,
-                fontFamily: 'Ubuntu',
-                fontSize: 35,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image(image: AssetImage('assets/images/appLogo2.png')),
+              Padding(
+                padding: EdgeInsets.all(30),
               ),
-            ),
-            RaisedButton(
-              child: Text('Continue'),
-              onPressed: () {
-                print("Pressed");
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MyApp()),
-                );
-              },
-            ),
-          ],
-        ),
+              Text(
+                'TreeTracker',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Ubuntu',
+                  fontSize: 35,
+                ),
+              )
+            ]),
       ),
     );
-    // );
   }
 }
