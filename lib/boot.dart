1b4074c780ca130968e83c52d40125dee478b2d6
Girish Rajani <girish.r.rajani@gmail.com>
2021-01-25 12:20:18 +0530

    Login Page File Added

--- lib/boot.dart
+++ lib/boot.dart
@@ -12,48 +12,6 @@ class Start extends StatelessWidget {
   }
 }
 
-// class BootScreen extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     // return MaterialApp(
-//     // home: Scaffold(
-//     return Scaffold(
-//       // appBar: AppBar(),
-//       backgroundColor: Colors.grey[900],
-//       body: Container(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             Image(image: AssetImage('assets/images/appLogo2.png')),
-//             Padding(
-//               padding: EdgeInsets.all(30),
-//             ),
-//             Text(
-//               'TreeTracker',
-//               style: TextStyle(
-//                 color: Colors.white,
-//                 fontFamily: 'Ubuntu',
-//                 fontSize: 35,
-//               ),
-//             ),
-//             // RaisedButton(
-//             //   child: Text('Continue'),
-//             //   onPressed: () {
-//             //     print("Pressed");
-//             //     Navigator.push(
-//             //       context,
-//             //       MaterialPageRoute(builder: (context) => MyApp()),
-//             //     );
-//             //   },
-//             // ),
-//           ],
-//         ),
-//       ),
-//     );
-//     // );
-//   }
-// }
-
 class BootScreen extends StatefulWidget {
   @override
   _BootScreenState createState() => _BootScreenState();
