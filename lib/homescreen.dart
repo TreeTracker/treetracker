cc84c231f203a59e370e6d3e1aae01f6d2973278
Harsh Koli <harshkoli201@gmail.com>
2021-01-27 12:03:55 +0530

    Homescreen design

---
+++ lib/homescreen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      HomeScreen()
+  );
+}
+
+
+class HomeScreen extends StatelessWidget {
+  final appTitle = 'HomeScreen';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
+    );
+  }
+}
+
+
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        backgroundColor: Colors.green,
+      ),
+      drawer: Drawer(
+          child: ListView(
+            children: [
+              DrawerHeader(
+              child: Text('TreeTracker logo'),
+              ),
+              ListTile(
+                leading: Icon(Icons.local_florist),
+                title: Text('My Trees'),
+                onTap: (){
+
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.wb_sunny),
+                title: Text('Climate Change'),
+                onTap: (){
+
+                },
+              ),
+                ListTile(
+                  leading: Icon(Icons.people),
+                  title: Text('About Us'),
+                  onTap: (){
+
+                  },
+                ),
+              ListTile(
+                leading: Icon(Icons.logout),
+                title: Text('Log out'),
+                onTap: (){
+
+                },
+              ),
+            ],
+          ),
+      ),
+    );
+  }
+}
+
