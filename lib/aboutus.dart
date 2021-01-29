5f390e81d69b57790457bb3d127c379c9429e985
vaishnavi bhalerao <vaishnavi.if0218@tpoly.in>
2021-01-29 13:07:43 +0530

    AboutusChanged

--- lib/aboutus.dart
+++ lib/aboutus.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.grey,
-          body: Center(
-            child: Image(
-              image: AssetImage('assets/images/tree2.png'),
-            ),
+void main() {
+  runApp(AboutUs());
+}
+
+class AboutUs extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey[900],
+        body: Center(
+          child: Image(
+            image: AssetImage('assets/images/tree2.png'),
           ),
-          appBar: AppBar(
-            title: Text('About Us'),
-            backgroundColor: Colors.lightGreen,
-          ),
+        ),
+        appBar: AppBar(
+          title: Text('About Us'),
+          backgroundColor: Colors.green,
         ),
       ),
     );
+  }
+}
