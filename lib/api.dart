17709dbad953e4f102c6498bd91383129b9a9c13
vaishnavi bhalerao <vaishnavi.if0218@tpoly.in>
2021-01-29 13:26:45 +0530

    AQI Page Created

--- lib/api.dart
+++ lib/api.dart
@@ -1,2 +1,20 @@
-import 'package:http/http.dart';
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(Aqi());
+}
+
+class Aqi extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          title: Text('AQI'),
+          backgroundColor: Colors.green,
+        ),
+      ),
+    );
+  }
+}
