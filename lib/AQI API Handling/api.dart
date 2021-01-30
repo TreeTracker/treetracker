b0937186a580ddbb2bc6f852eb48d989807f27b1
Girish Rajani <girish.r.rajani@gmail.com>
2021-01-30 14:54:09 +0530

    AQI API Handled

--- lib/AQI API Handling/api.dart
+++ lib/AQI API Handling/api.dart
@@ -1,11 +1,13 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 
-void aqiOfCities(List<String> arguments) async {
+void main(List<String> arguments) async {
   var url = '';
   var response = await http.get(url);
   if (response.statusCode == 200) {
     var jsonResponse = convert.jsonDecode(response.body);
+    print(jsonResponse["data"]["current"]["pollution"]["aqius"]);
+    return;
   } else {
     print('Request failed with status: ${response.statusCode}.');
   }
