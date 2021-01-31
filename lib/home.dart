c07a7776afa7650891c95f8a49b3f104345dfc10
Harsh Koli <harshkoli201@gmail.com>
2021-01-31 14:57:41 +0530

    Sidebar and camera icon added

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:treetracker/AQI%20API%20Handling/aqi.dart';
 
 void main(List<String> args) {
   runApp(
@@ -23,6 +24,12 @@ class _AppHomePageState extends State<AppHomePage> {
           title: Text('TreeTracker'),
           backgroundColor: Colors.grey[900],
           centerTitle: true,
+          actions: [
+            IconButton(
+              icon: Icon(Icons.camera_alt_rounded),
+              onPressed: () {},
+            ),
+          ],
         ),
         body: Column(
           children: <Widget>[
@@ -37,6 +44,58 @@ class _AppHomePageState extends State<AppHomePage> {
               ),
             ),
           ],
+        ),
+        drawer: Drawer(
+          child: Container(
+            color: Colors.grey[600],
+            child: ListView(
+              children: [
+                DrawerHeader(
+                  child: Column(
+                    children: <Widget>[
+                      // CircleAvatar(
+                      //   radius: 40,
+                      //   backgroundColor: Colors.green,
+                      //   backgroundImage:
+                      //       NetworkImage(ouruser.photoURL.toString()),
+                      // ),
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                      ),
+                      // Text(ouruser.displayName),
+                    ],
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(Icons.help_center_rounded),
+                  title: Text('Help'),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Icon(Icons.people),
+                  title: Text('About Us'),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Icon(Icons.logout),
+                  title: Text('Log out'),
+                  onTap: () {
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(builder: (context) => Auth()),
+                    // );
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.exit_to_app),
+                  title: Text('Exit'),
+                  onTap: () {
+                    // exit(0);
+                  },
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
