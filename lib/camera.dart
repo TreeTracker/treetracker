fb2a3a1e5961dbdfe98e2f2a3322d04e458ae5e2
Harsh Koli <harshkoli201@gmail.com>
2021-04-11 00:48:48 +0530

    camera storage

--- lib/camera.dart
+++ lib/camera.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -26,13 +27,6 @@ class _LandingScreenState extends State<LandingScreen> {
   File _image;
   Position _currentPosition;
 
-  Future getImage() async {
-    final image = await ImagePicker.pickImage(source: ImageSource.camera);
-    setState(() {
-      _image = image;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,12 +35,6 @@ class _LandingScreenState extends State<LandingScreen> {
       ),
       body: ListView(
         children: [
-          Container(
-            child: _image == null
-                ? Text("Image is not Loaded",
-                    style: TextStyle(color: Colors.black, fontSize: 20))
-                : enableUpload(),
-          ),
           TextField(
             decoration: InputDecoration(border: OutlineInputBorder()),
           ),
@@ -97,7 +85,7 @@ class _LandingScreenState extends State<LandingScreen> {
               color: Colors.blue,
               onPressed: () {
                 Reference ref =
-                    FirebaseStorage.instance.ref().child('harsh.jpg');
+                    FirebaseStorage.instance.ref().child('girish.jpg');
                 ref.putFile(_image);
 
                 return "";
@@ -117,4 +105,115 @@ class _LandingScreenState extends State<LandingScreen> {
       print(e);
     });
   }
+
+  Widget _buildBody(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('storage').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return LinearProgressIndicator();
+
+        return _buildList(context, snapshot.data.documents);
+      },
+    );
+  }
+
+  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+    return ListView(
+        padding: const EdgeInsets.only(top: 20.0),
+        children:
+            snapshot.map((data) => _buildListItem(context, data)).toList());
+  }
+
+  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+    final record = Record.fromSnapshot(data);
+
+    return Padding(
+      key: ValueKey(record.location),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: ListTile(
+          title: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  record.location,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+              ),
+              Image.network(record.url),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future getImage() async {
+    // Get image from gallery.
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+    _uploadImageToFirebase(image);
+  }
+
+  Future<void> _uploadImageToFirebase(File image) async {
+    try {
+      // Make random image name.
+      int randomNumber = Random().nextInt(100000);
+      String imageLocation = 'images/image${randomNumber}.jpg';
+
+      // Upload image to firebase.
+      final Reference storageReference =
+          FirebaseStorage().ref().child(imageLocation);
+      final UploadTask uploadTask = storageReference.putFile(image);
+      await uploadTask;
+      _addPathToDatabase(imageLocation);
+    } catch (e) {
+      print(e.message);
+    }
+  }
+
+  Future<void> _addPathToDatabase(String text) async {
+    try {
+      // Get image URL from firebase
+      final ref = FirebaseStorage().ref().child(text);
+      var imageString = await ref.getDownloadURL();
+
+      // Add location and url to database
+      await Firestore.instance
+          .collection('storage')
+          .document()
+          .setData({'url': imageString, 'location': text});
+    } catch (e) {
+      print(e.message);
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              content: Text(e.message),
+            );
+          });
+    }
+  }
+}
+
+class Record {
+  final String location;
+  final String url;
+  final DocumentReference reference;
+
+  Record.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['location'] != null),
+        assert(map['url'] != null),
+        location = map['location'],
+        url = map['url'];
+
+  Record.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data(), reference: snapshot.reference);
+
+  @override
+  String toString() => "Record<$location:$url>";
 }
